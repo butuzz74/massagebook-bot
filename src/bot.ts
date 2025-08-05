63f4f720f864881c7dd600edf55e2a565a2375e9
Alexey Danilchik <butuzz74@gmail.com>
2025-08-05 21:11:01 +0300

    fixed cron

--- src/bot.ts
+++ src/bot.ts
@@ -26,7 +26,7 @@ cron.schedule("0 10 * * *", async () => {
     await cleanOldWorkingDays();
 });
 
-cron.schedule("0 20 * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
     await connectDb();
 
     const date = new Date();
